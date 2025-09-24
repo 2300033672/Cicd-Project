846657bd16a7ebcaaf840a39fe927681ab922947
VAVILALA SHANMUKHA PRASANNA DATTASAI <162683779+2300033672@users.noreply.github.com>
2025-09-24 23:40:02 +0530

    Update SpringBootSdpProjectApplication.java

--- SpringBootSDPProject/src/main/java/com/klef/fsd/sdp/SpringBootSdpProjectApplication.java
+++ SpringBootSDPProject/src/main/java/com/klef/fsd/sdp/SpringBootSdpProjectApplication.java
@@ -1,14 +1,12 @@
-package com.klef.fsd.sdp;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
-public class SpringBootSdpProjectApplication {
+public class SpringBootSdpProjectApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootSdpProjectApplication.class, args);
-		System.out.println("SDP Project is running.....");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSdpProjectApplication.class, args);
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringBootSdpProjectApplication.class);
+    }
 }
